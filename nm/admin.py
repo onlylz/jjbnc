d01594ea7d799a28d0adc11671dd9e0d8d02f2e8
83042609@qq.com <Lz123456>
2017-04-26 22:10:37 +0800

    170426@home

--- nm/admin.py
+++ nm/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Device)
+admin.site.register(AuthProfile)
+admin.site.register(Port)
+admin.site.register(SubPort)
+admin.site.register(Vendor)
+admin.site.register(VendorModel)
+admin.site.register(SnmpCommunity)
--- nm/models.py
+++ nm/models.py
@@ -1,41 +1,89 @@
 from django.db import models
 
-class Device(models.Model):
-    vendor = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
 
-    hostname = models.CharField(max_length=100)
-    mgmt_ip = models.CharField(max_length=100)
-    interface = models.ForeignKey(Interface)
-
-    auth_schema = models.ForeignKey(AuthSchema)
-    snmp_community = models.ForeignKey(SnmpCommunity)
-
-
-class Interface(models.Model):
+class SubPort(models.Model):
     name = models.CharField(max_length=100)
-    ip = models.CharField(max_length=100)
-    sub_interface = models.ForeignKey(SubInterface)
+    ip = models.GenericIPAddressField(null=True, blank=True)
 
-class SubInterface(models.Model):
+    def __str__(self):
+        return self.port.device.hostname + '_' + self.port.name + '_' + self.name
+
+
+class Port(models.Model):
     name = models.CharField(max_length=100)
-    ip = models.CharField(max_length=100)
+    ip = models.GenericIPAddressField(null=True, blank=True)
+    sub_port = models.OneToOneField(SubPort, blank=True, null=True)
+
+    def __str__(self):
+        return self.device.hostname + '_' + self.name
 
 
 class Vendor(models.Model):
+    name = models.CharField(max_length=20)
+    full_name = models.CharField(max_length=100)
+    chinese_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
+class VendorModel(models.Model):
+    name = models.CharField(max_length=100)
+    vendor = models.ForeignKey(Vendor)
 
-class Model(models.Model):
+    def __str__(self):
+        return self.vendor.name + '_' + self.name
 
 
-class AuthSchema(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    super_password = models.CharField(max_length=100)
+class AuthProfile(models.Model):
+    ACCESS_METHOD=(
+        ('ssh', 'SSH'),
+        ('telnet', 'Telnet'),
+    )
+    name = models.CharField(max_length=50)
+    access_method = models.CharField(max_length=20, choices=ACCESS_METHOD, default='telnet')
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    super_password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
 
 class SnmpCommunity(models.Model):
+    VERSION=(
+        ('v2', 'V2'),
+        ('v2c', 'V2c'),
+        ('v3', 'V3'),
+    )
+    name = models.CharField(max_length=100, null=False)
+    version = models.CharField(max_length=20, choices=VERSION)
+    ro = models.CharField(max_length=50)
+    rw = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Device(models.Model):
+    #vendor = models.CharField(max_length=100)
+    model = models.ForeignKey(VendorModel)
+    #type = models.CharField(max_length=100)
+
+    hostname = models.CharField(max_length=100)
+    mgmt_ip = models.GenericIPAddressField(blank=True, null=True)
+    port = models.OneToOneField(Port, blank=True, null=True)
+    auth_profile = models.ForeignKey(AuthProfile, blank=True, null=True)
+    snmp_community = models.ForeignKey(SnmpCommunity, blank=True, null=True)
+
+    def __str__(self):
+        return self.hostname
+
+
+
+
+
+
 
 
 
